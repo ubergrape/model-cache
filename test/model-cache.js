363855eb178a5e63e1bb1007488452d74ba30c18
Arpad Borsos <arpad.borsos@googlemail.com>
2014-01-22 12:13:45 +0100

    allow clearing selective entries

--- test/model-cache.js
+++ test/model-cache.js
@@ -26,6 +26,17 @@ describe('Cache', function () {
 		var instance = new SomeModel({prop: 'key', prop2: 'other'});
 		should.exist(SomeModel.get('key'));
 		SomeModel.clear();
-		should.not.exist(SomeModel.get('foo'));
+		should.not.exist(SomeModel.get('key'));
+	});
+	it('should allow clearing selected items from the cache', function () {
+		var SomeModel = new Model(['prop'])
+			.use(Cache('prop'));
+		var instance1 = new SomeModel({prop: 'key1', prop2: 'other'});
+		var instance2 = new SomeModel({prop: 'key2', prop2: 'other'});
+		should.exist(SomeModel.get('key1'));
+		should.exist(SomeModel.get('key2'));
+		SomeModel.clear('key1');
+		should.not.exist(SomeModel.get('key1'));
+		should.exist(SomeModel.get('key2'));
 	});
 });
